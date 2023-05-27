729052fe9fba9b2bde392a8abdb85bde07390554
Guillermo <guillermotunes@hotmail.com>
2023-05-27 16:34:02 -0300

    Creacion de la 3 pre-entrega a nombre de Tunes G.

--- project/config/settings.py
+++ project/config/settings.py
@@ -15,6 +15,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import sys
+APLICACIONES = BASE_DIR / "apps"
+sys.path.append(str(APLICACIONES))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -36,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles', "home", 
+
 ]
 
 MIDDLEWARE = [
